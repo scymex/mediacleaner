40881780f3494713515482eac0a6865609f49090
Tamás Verseczi <verseczi@gmail.com>
2018-04-30 23:22:58 +0200

    -Fixed: if you changed the media server you had restart the whole application to make the settings take effect.

--- MediaCleaner/MediaServer.cs
+++ MediaCleaner/MediaServer.cs
@@ -5,33 +5,49 @@ namespace MediaCleaner
     public class MediaServer:IMediaServer
     {
         IMediaServer mServer;
+        private int mSType;
 
         public MediaServer()
         {
-            switch(Config.MediaServer)
+            initMediaServer();
+        }
+
+        private void initMediaServer()
+        {
+            switch (Config.MediaServer)
             {
                 case 0:
                     mServer = new Plex.Plex();
-                break;
+                    break;
                 case 1:
                     mServer = new Emby.Emby();
-                break;
-
+                    break;
             }
+
+            mSType = Config.MediaServer;
+        }
+
+        private void checkMediaServer()
+        {
+            if (mSType != Config.MediaServer)
+                initMediaServer();
         }
 
         public bool checkConnection()
         {
+            checkMediaServer();
             return mServer.checkConnection();
         }
 
         public Episode getItem(string EpisodePath)
         {
+            checkMediaServer();
             return mServer.getItem(EpisodePath);
         }
 
         public bool checkSettings()
         {
+            checkMediaServer();
             return mServer.checkSettings();
         }
     }
